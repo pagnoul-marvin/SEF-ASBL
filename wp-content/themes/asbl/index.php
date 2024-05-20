340cd81e9ccd2c259c7c84c0ebf75ce175d67186
pagnoul-marvin <marvin.pagnoul@student.hepl.be>
2024-05-20 12:02:49 +0200

    Sections styles done

--- wp-content/themes/asbl/index.php
+++ wp-content/themes/asbl/index.php
@@ -5,10 +5,23 @@ get_header();
 
 <main>
 
-    <section>
-        <h2><?= get_field('who_are_we_title') ?></h2>
-        <p><?= get_field('who_are_we_text') ?></p>
-        <a href="<?= esc_url(get_field('who_are_we_link')['url']); ?>"><?= esc_html(get_field('who_are_we_link')['title']); ?></a>
+    <section class="odd spacing">
+
+        <h2 class="section_second_title"><?= get_field('who_are_we_title') ?></h2>
+
+        <img class="filter" src="<?= get_field('who_are_we_image')['url'] ?>"
+             alt="<?= get_field('who_are_we_image')['alt'] ?>">
+
+        <div class="center">
+
+            <p class="text"><?= get_field('who_are_we_text') ?></p>
+
+        </div>
+
+            <a class="section_link" title="Aller vers la page Qui sommes-nous ?" href="<?= esc_url(get_field('who_are_we_link')['url']); ?>"><?= esc_html(get_field('who_are_we_link')['title']); ?></a>
+
+            <p class="section_link_hover"><?= esc_html(get_field('who_are_we_link')['title']); ?></p>
+
     </section>
 
 </main>
